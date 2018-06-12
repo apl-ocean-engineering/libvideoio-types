5f23516d14c4a07baa6b705495424aa78e5d51f7
Aaron Marburg <aaron@apl.washington.edu>
2018-06-12 10:37:08 -0700

    Extended automatic image conversion.

--- lib/ImageSource.cpp
+++ lib/ImageSource.cpp
@@ -8,16 +8,16 @@ namespace libvideoio {
     int ret = getRawImage(i,mat);
     if( _outputType <0 || mat.type() == _outputType ) return ret;
 
-    auto inChannels  = ((mat.type()  & ~CV_MAT_DEPTH_MASK) >> CV_CN_SHIFT) + 1;
+    //auto inChannels  = ((mat.type()  & ~CV_MAT_DEPTH_MASK) >> CV_CN_SHIFT) + 1;
     auto outChannels = ((_outputType & ~CV_MAT_DEPTH_MASK) >> CV_CN_SHIFT) + 1;
 
     if( outChannels == 3 ) {
-      CHECK( cvtToRGB() >= 0 ) << "No conversion to RGB specified";
+      CHECK( cvtToRGB() >= 0 ) << "No conversion to RGB specified by ImageSource";
       cv::Mat tmp;
       cv::cvtColor( mat, tmp, cvtToRGB() );
       tmp.convertTo( mat, _outputType );
     } else if( outChannels == 1 ) {
-      CHECK( cvtToGray() >= 0 ) << "No conversion to gray specific";
+      CHECK( cvtToGray() >= 0 ) << "No conversion to gray specific by ImageSource";
       cv::Mat tmp;
       cv::cvtColor( mat, tmp, cvtToGray() );
       tmp.convertTo( mat, _outputType );
