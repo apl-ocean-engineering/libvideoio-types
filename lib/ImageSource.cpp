af9e8fd48385676240389f730968dc3b7825edc1
Aaron Marburg <amarburg@apl.washington.edu>
2018-06-12 09:48:16 -0700

    Initial commit of automatic conversion in ImageSource.

--- lib/ImageSource.cpp
+++ lib/ImageSource.cpp
@@ -8,8 +8,22 @@ namespace libvideoio {
     int ret = getRawImage(i,mat);
     if( _outputType <0 || mat.type() == _outputType ) return ret;
 
+    auto inChannels  = ((mat.type()  & ~CV_MAT_DEPTH_MASK) >> CV_CN_SHIFT) + 1;
+    auto outChannels = ((_outputType & ~CV_MAT_DEPTH_MASK) >> CV_CN_SHIFT) + 1;
 
-    // Else attempt automatic conversion
+    if( outChannels == 3 ) {
+      CHECK( cvtToRGB() >= 0 ) << "No conversion to RGB specified";
+      cv::Mat tmp;
+      cv::cvtColor( mat, tmp, cvtToRGB() );
+      tmp.convertTo( mat, _outputType );
+    } else if( outChannels == 1 ) {
+      CHECK( cvtToGray() >= 0 ) << "No conversion to gray specific";
+      cv::Mat tmp;
+      cv::cvtColor( mat, tmp, cvtToGray() );
+      tmp.convertTo( mat, _outputType );
+    } else {
+      LOG(FATAL) << "Unable to figure out how to convert to " << outChannels << " channels";
+    }
 
     return ret;
   }
